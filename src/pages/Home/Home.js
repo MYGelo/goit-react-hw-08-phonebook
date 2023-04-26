d666d09974972a885dcb49e52cf80e71605ce906
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-04-26 23:24:37 +0300

    1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,25 +5,18 @@ export default function Home() {
     <div className={css.container}>
       <p>
         hello, plase{' '}
-        <a
-          className={css.link}
-          href="http://localhost:3000/goit-react-hw-08-phonebook/login"
-        >
+        <a className={css.link} href="http:/goit-react-hw-08-phonebook/login">
           login
         </a>{' '}
         or{' '}
         <a
           className={css.link}
-          href="http://localhost:3000/goit-react-hw-08-phonebook/register"
+          href="http:/goit-react-hw-08-phonebook/register"
         >
           register
         </a>{' '}
         new phonebook list
       </p>
-      {/* <div>
-        <p>mango@mail.com</p>
-        <button>Logout</button>
-      </div> */}
     </div>
   );
 }
