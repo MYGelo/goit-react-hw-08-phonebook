9a5a1ac1b9836bfdb30e6e33a17dce0071559c1e
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-04-26 23:39:40 +0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,32 @@
+import { useAuth } from 'hooks';
 import css from './Home.module.css';
 
 export default function Home() {
+  const { isLoggedIn, user } = useAuth();
+
   return (
     <div className={css.container}>
-      <p>
-        hello, plase{' '}
-        <a className={css.link} href="http:/goit-react-hw-08-phonebook/login">
-          login
-        </a>{' '}
-        or{' '}
-        <a
-          className={css.link}
-          href="http:/goit-react-hw-08-phonebook/register"
-        >
-          register
-        </a>{' '}
-        new phonebook list
-      </p>
+      {isLoggedIn ? (
+        <p>
+          {user.name}, to see the list of contacts, go to the{' '}
+          <a href="http:/goit-react-hw-08-phonebook/contacts">contacts</a>
+        </p>
+      ) : (
+        <p>
+          hello, plase{' '}
+          <a className={css.link} href="http:/goit-react-hw-08-phonebook/login">
+            login
+          </a>{' '}
+          or{' '}
+          <a
+            className={css.link}
+            href="http:/goit-react-hw-08-phonebook/register"
+          >
+            register
+          </a>{' '}
+          new phonebook list
+        </p>
+      )}
     </div>
   );
 }
